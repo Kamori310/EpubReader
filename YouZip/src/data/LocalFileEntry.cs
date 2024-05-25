b0c19f7593a69e57374d9019a6a35198705884d9
retterrudi <paul.serdack+retterrudi@gmail.com>
2024-05-25 10:32:39 +0200

    Change stuff

--- YouZip/src/data/LocalFileEntry.cs
+++ YouZip/src/data/LocalFileEntry.cs
@@ -1,7 +1,17 @@
 ﻿namespace YouZip.data;
 
-public class LocalFileEntry(LocalFileHeader localFileHeader, byte[] localFileData)
+public class LocalFileEntry
 {
-    private LocalFileHeader _localFileHeader = localFileHeader;
-    private byte[] _localFileData = localFileData;
+    private LocalFileHeader _localFileHeader;
+    private byte[] _localFileData;
+
+    public LocalFileEntry (byte[] input, int startingPosition)
+    {
+        _localFileHeader = new LocalFileHeader(input, startingPosition);
+        // TODO: Add class for localFileData and construct with info from header
+        _localFileData = input.Subarray(
+            startingPosition + _localFileHeader.HeaderLength(), 
+            (int)_localFileHeader.CompressedSize.ToUInt());
+    }
+
 }
