54a7f80e22ed3380602f62a3fb84431cb3d33f53
retterrudi <paul.serdack+retterrudi@gmail.com>
2024-06-03 10:53:55 +0200

    Refactor LocalFileHeader class and its tests

--- TestYouZip/TestLocalFileHeader.cs
+++ TestYouZip/TestLocalFileHeader.cs
@@ -23,51 +23,51 @@ public class TestLocalFileHeader
             0x00, 0x00
         ], 0);
         
-        List<byte[]> input = new List<byte[]>()
-        {
-            new byte[] { 0b1000_0000, 0b0000_0000 },
-            new byte[] { 0b0100_0000, 0b0000_0000 },
-            new byte[] { 0b0010_0000, 0b0000_0000 },
-            new byte[] { 0b0001_0000, 0b0000_0000 },
-            new byte[] { 0b0000_1000, 0b0000_0000 },
-            new byte[] { 0b0000_0100, 0b0000_0000 },
-            new byte[] { 0b0000_0010, 0b0000_0000 },
-            new byte[] { 0b0000_0001, 0b0000_0000 },
-            new byte[] { 0b0000_0000, 0b1000_0000 },
-            new byte[] { 0b0000_0000, 0b0100_0000 },
-            new byte[] { 0b0000_0000, 0b0010_0000 },
-            new byte[] { 0b0000_0000, 0b0001_0000 },
-            new byte[] { 0b0000_0000, 0b0000_1000 },
-            new byte[] { 0b0000_0000, 0b0000_0100 },
-            new byte[] { 0b0000_0000, 0b0000_0010 },
-            new byte[] { 0b0000_0000, 0b0000_0001 },
-        };
+        List<byte[]> input =
+        [
+            [0b1000_0000, 0b0000_0000],
+            [0b0100_0000, 0b0000_0000],
+            [0b0010_0000, 0b0000_0000],
+            [0b0001_0000, 0b0000_0000],
+            [0b0000_1000, 0b0000_0000],
+            [0b0000_0100, 0b0000_0000],
+            [0b0000_0010, 0b0000_0000],
+            [0b0000_0001, 0b0000_0000],
+            [0b0000_0000, 0b1000_0000],
+            [0b0000_0000, 0b0100_0000],
+            [0b0000_0000, 0b0010_0000],
+            [0b0000_0000, 0b0001_0000],
+            [0b0000_0000, 0b0000_1000],
+            [0b0000_0000, 0b0000_0100],
+            [0b0000_0000, 0b0000_0010],
+            [0b0000_0000, 0b0000_0001]
+        ];
         
-        List<bool[]> desiredOutpout = new List<bool[]>()
-        {
-            new bool[] {true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            new bool[] {false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false}, 
-            new bool[] {false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false},
-            new bool[] {false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false},
-            new bool[] {false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false},
-            new bool[] {false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false},
-            new bool[] {false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false},
-            new bool[] {false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false},
-            
-            new bool[] {false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false},
-            new bool[] {false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false},
-            new bool[] {false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false},
-            new bool[] {false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false},
-            new bool[] {false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false},
-            new bool[] {false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false},
-            new bool[] {false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false},
-            new bool[] {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true},
-        };
+        List<bool[]> desiredOutput =
+        [
+            [true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, true, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, true, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, true, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, true, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false],
+
+            [false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, true, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, true, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, true, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, true, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true]
+        ];
 
         for (var i = 0; i < 16; i++)
         {
             localFileHeader.GeneralPurposeBitFlag = input[i];
-            Assert.That(localFileHeader.GetGeneralPurposeBitFlags(), Is.EqualTo(desiredOutpout[i]));
+            Assert.That(localFileHeader.GetGeneralPurposeBitFlags(), Is.EqualTo(desiredOutput[i]));
         }
     }
 
